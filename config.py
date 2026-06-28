cc0d896a980970621963f30aa72d8256151b61b8
JoaoPedro-SA <antunesjoaopedro3@gmail.com>
2026-06-28 16:12:41 -0300

    erro do modelo beta

--- config.py
+++ config.py
@@ -9,6 +9,17 @@ app.config['HOST'] ='0.0.0.0'
 app.config['PORT'] = int(os.environ.get('PORT', 5000))
 app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
 
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = os.environ.get(
+        "CORS_ALLOW_ORIGIN",
+        "*",
+    )
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    return response
+
 # URL base para o ambiente local
 if app.config['HOST'] == '0.0.0.0':
     BASE_URL = "http://127.0.0.1:" + str(app.config['PORT'])
